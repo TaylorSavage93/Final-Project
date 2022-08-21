d7a9104082471f16593a8028d67b94795a29d83e
Taylor Savage <taylorsavage93@gmail.com>
2022-08-21 16:56:46 -0400

    fixed final project

---
+++ BookClubWeb/Models/Books.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookClubWeb.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Title { get; set; }
+    }
+}
